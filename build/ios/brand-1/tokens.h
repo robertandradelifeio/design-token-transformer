4354d9c70b6aa5ee4470957e9dc7f66ab0a3ef0e
Robert Andrade <robert.andrade@life.io>
2022-07-15 15:01:26 -0500

    adds multi brand config file

---
+++ build/ios/brand-1/tokens.h
@@ -0,0 +1,40 @@
+
+//
+// tokens.h
+//
+
+// Do not edit directly
+// Generated on Fri, 15 Jul 2022 19:56:26 GMT
+
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#define ColorBrandPrimary [UIColor colorWithRed:0.231f green:0.349f blue:0.596f alpha:1.000f]
+#define ColorBrandSecondary [UIColor colorWithRed:0.259f green:0.404f blue:0.698f alpha:1.000f]
+#define ColorBaseBlack [UIColor colorWithRed:0.000f green:0.000f blue:0.000f alpha:1.000f]
+#define ColorBaseGrayLight [UIColor colorWithRed:0.800f green:0.800f blue:0.800f alpha:1.000f]
+#define ColorBaseGrayMedium [UIColor colorWithRed:0.600f green:0.600f blue:0.600f alpha:1.000f]
+#define ColorBaseGrayDark [UIColor colorWithRed:0.067f green:0.067f blue:0.067f alpha:1.000f]
+#define ColorBaseRed [UIColor colorWithRed:1.000f green:0.000f blue:0.000f alpha:1.000f]
+#define ColorBaseGreen [UIColor colorWithRed:0.000f green:1.000f blue:0.000f alpha:1.000f]
+#define ColorPrimary [UIColor colorWithRed:0.231f green:0.349f blue:0.596f alpha:1.000f]
+#define ColorSecondary [UIColor colorWithRed:0.259f green:0.404f blue:0.698f alpha:1.000f]
+#define ColorActionPrimary [UIColor colorWithRed:0.231f green:0.349f blue:0.596f alpha:1.000f]
+#define ColorActionSecondary [UIColor colorWithRed:0.259f green:0.404f blue:0.698f alpha:1.000f]
+#define ColorActionTertiary [UIColor colorWithRed:0.600f green:0.600f blue:0.600f alpha:1.000f]
+#define ColorFontBase [UIColor colorWithRed:0.000f green:0.000f blue:0.000f alpha:1.000f]
+#define ColorFontPrimary [UIColor colorWithRed:0.231f green:0.349f blue:0.596f alpha:1.000f]
+#define ColorFontSecondary [UIColor colorWithRed:0.259f green:0.404f blue:0.698f alpha:1.000f]
+#define ColorFontTertiary [UIColor colorWithRed:0.800f green:0.800f blue:0.800f alpha:1.000f]
+#define ButtonBorderRadius 4px
+#define ButtonTextSize 16.00f
+#define ButtonHeight 50px
+#define FontFamilyHeaders @"Montserrat"
+#define FontFamilyBase @"San Francisco"
+#define FontPlatformSystem @"San Francisco"
+#define SizeFontSmall 12.00f
+#define SizeFontMedium 16.00f
+#define SizeFontLarge 32.00f
+#define SizeFontBase 16.00f
+
